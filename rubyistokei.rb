fa55f44f1d2ebe82284bafd8eda1a2dc17fdb657
Shimpei Makimoto <makimoto@tsuyabu.in>
2013-05-28 23:03:38 +0900

    Glitch photos

--- rubyistokei.rb
+++ rubyistokei.rb
@@ -4,12 +4,14 @@ Bundler.require
 require 'yaml'
 require 'json'
 require 'digest/sha1'
+require 'open-uri'
 
 class Database
   def initialize(path)
     data_loaded = Dir[File.join(path, '*.yaml')].map do |yaml_path|
       hash = YAML.load_file(yaml_path)
       id = File.basename(yaml_path, '.yaml')
+      hash['url'] = "/glitch?url=#{hash['url']}"
       hash.merge(id: id)
     end
     @data = data_loaded.sort_by do |entry|
@@ -30,6 +32,14 @@ module Rubyistokei
 
     get '/' do
       haml :index
+    end
+
+    get '/glitch' do
+      data = open(params[:url]).read
+      data_a = data[0 .. data.size / 2]
+      data_b = data[data.size / 2 .. -1]
+      content_type :jpeg
+      data_a + data_b.force_encoding('ascii-8bit').gsub('a', 'b')
     end
 
     get '/css/screen.css' do
